48f2a3da67d00fac0c783b75f0d7e41daffd5198
jarvma18 <markusmjarvinen@outlook.com>
2024-03-04 23:58:24 +0200

    Removed console.log

--- app/data/access/test.ts
+++ app/data/access/test.ts
@@ -3,6 +3,5 @@ const database = require('../database');
 export async function createTest(body: any) {
   body.created_at = new Date();
   const test = await database.knex('test').insert(body).returning('*');;
-  console.log(test[0]);
   return test[0];
 };
